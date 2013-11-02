a97e58bbb0bd409429f048c4af932c199b0e524e
Edvin Malinovskis <edvin.malinovskis@gmail.com>
2013-11-02 10:13:34 +0000

    Package boilerplate

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='OpenCitiesChallenge',
+    url='RODO',
+    version='0.1dev',
+    packages=['opencitieschallenge',],
+    author='Keir Smith, Edvin Malinovskis, Sean Jacobson, Felix, Julia',
+    author_email='TODO',
+    license='TODO',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        ],
+    long_description=open('README').read(),
+)
