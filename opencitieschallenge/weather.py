4c6b5ba110332c61aabdf828081ed12a8865ad6f
Keir Smith <kaffse@gmail.com>
2013-11-02 17:29:18 +0000

    Except for values that aren't valid, weather.py should be done

--- opencitieschallenge/weather.py
+++ opencitieschallenge/weather.py
@@ -1,6 +1,40 @@
 import requests, time
 import xml.etree.ElementTree as ET
 
+weather_type = [
+"Not available",
+"Clear night",
+"Sunny day",
+"Partly cloudy (night)",
+"Partly cloudy (day)",
+"Not used",
+"Mist",
+"Fog",
+"Cloudy",
+"Overcast",
+"Light rain shower (night)",
+"Light rain shower (day)",
+"Drizzle",
+"Light rain",
+"Heavy rain shower (night)",
+"Heavy rain shower (day)",
+"Heavy rain",
+"Sleet shower (night)",
+"Sleet shower (day)",
+"Sleet",
+"Hail shower (night)",
+"Hail shower (day)",
+"Hail",
+"Light snow shower (night)",
+"Light snow shower (day)",
+"Light snow",
+"Heavy snow shower (night)",
+"Heavy snow shower (day)",
+"Heavy snow",
+"Thunder shower (night)",
+"Thunder shower (day)", 
+"Thunder"]
+
 def get_data():
 	r = requests.get('http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/xml/310009?res=3hourly&key=d73af5c0-e626-4591-ab9b-ef36f4d8956c')
 
@@ -39,13 +73,9 @@ def get_weather(root, in_date, in_time):
 	weth = []
 
 	for n in wd:
-		 weth.append((n + ": " + rw.attrib[wd[n]['name']] + " " + wd[n]['units']))
+		if (n == "Weather Type"):
+			weth.append(n + ": " + weather_type[(int)(rw.attrib[wd[n]['name']])])
+
+		weth.append((n + ": " + rw.attrib[wd[n]['name']] + " " + wd[n]['units']))
 	
 	return weth
-
-def get_general_weather(root, in_date, in_time):
-	wd = get_params(root)
-
-	rw = get_raw_weather(root, in_date, in_time)
-
-	
