d25fa34dcb68d0a1a00958d1a91b7d67c425c0ce
Edvin Malinovskis <edvin.malinovskis@gmail.com>
2013-11-03 00:43:00 +0000

    stpped tracking old files

--- opencitieschallenge/SQLite/users.sql
+++
@@ -1,3 +0,0 @@
-BEGIN TRANSACTION;
-CREATE TABLE Users(UId integer primary key autoincrement, Email text, SId text, LastAccess text, password text);
-COMMIT
--- opencitieschallenge/SQLite/visited_venues.sql
+++
@@ -1,3 +0,0 @@
-BEGIN TRANSACTION;
-CREATE TABLE Venues(UId integer foreign key, 4Id text primary key, WeatherType integer, Duration integer);
-COMMIT
